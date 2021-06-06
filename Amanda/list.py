840206927b43e9fa31c03073113b952b42800238
Gioia <AGioia@HED.Design>
2021-06-05 20:43:23 -0500

    Update list.py

--- Amanda/list.py
+++ Amanda/list.py
@@ -1,10 +1,12 @@
 cities_lived = ['Montreal', 'San Francisco', 'Austin']
 populations = [500, 700, 800]
 cities = [cities_lived, populations]
+
 number_of_cities = len(cities_lived)
-#if number_of_cities <= 10:
-   # for i in range(0, number_of_cities):
-       # print(cities_lived[i])
+
+if number_of_cities <= 10:
+   for i in range(0, number_of_cities):
+       print(cities_lived[i])
 
 
 for i in range(0, number_of_cities):
